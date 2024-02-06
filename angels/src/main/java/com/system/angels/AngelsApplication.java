825e88e7a656fc09615c974cd2cf58a0645ee2d8
Nome <miranhaomi@gmail.com>
2024-02-06 19:08:06 -0300

    Initial commit

---
+++ angels/src/main/java/com/system/angels/AngelsApplication.java
@@ -0,0 +1,13 @@
+package com.system.angels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelsApplication.class, args);
+	}
+
+}
---
+++ angels/src/test/java/com/system/angels/AngelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.angels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
