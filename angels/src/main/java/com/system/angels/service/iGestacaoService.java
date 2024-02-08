1617c8298d6f5467ac2c4d6d560c8868ab2d7490
elise <Elisenovaes10@gmail.com>
2024-02-08 18:42:58 -0300

    Alteracão em iGestacaoService

--- angels/src/main/java/com/system/angels/service/iGestacaoService.java
+++ angels/src/main/java/com/system/angels/service/iGestacaoService.java
@@ -1,6 +1,7 @@
 package com.system.angels.service;
 
 import com.system.angels.domain.Gestante;
+import com.system.angels.dto.AtualizarGestacaoDTO;
 import com.system.angels.dto.GestacaoCadastroDTO;
 import com.system.angels.dto.VisualizarGestacaoDTO;
 import java.util.List;
@@ -8,17 +9,17 @@ import java.util.List;
 public interface iGestacaoService {
 
     public GestacaoCadastroDTO adicionarGestacao(GestacaoCadastroDTO gestacaoDTO);
-
-    public GestacaoCadastroDTO salvarGestacao(GestacaoCadastroDTO gestacaoDTO);
-
+    
     public VisualizarGestacaoDTO obterGestacaoPorId(Long id);
-
+    
     public boolean gestacaoExiste(Long id);
-
+    
     public VisualizarGestacaoDTO obterGestacaoPorGestante(Gestante gestante);
-
+    
     public List<VisualizarGestacaoDTO> obterTodasGestacoes();
-
+    
+    public AtualizarGestacaoDTO atualizarGestacao(AtualizarGestacaoDTO gestacaoDTO);
+    
     public void deletarGestacao(Long id);
 
 }
