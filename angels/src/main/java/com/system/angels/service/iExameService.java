6f1a3de3f9c5d692e411b97f1fc4695ad5ee455e
Nome <miranhaomi@gmail.com>
2024-02-08 18:49:34 -0300

    Criando o iExameService e de cara a implementação das funções criadas na interface, mas sem a funcionalidade feita ainda.

--- angels/src/main/java/com/system/angels/service/iExameService.java
+++ angels/src/main/java/com/system/angels/service/iExameService.java
@@ -1,4 +1,15 @@
 package com.system.angels.service;
 
+import com.system.angels.dto.AtualizarExameDTO;
+import com.system.angels.dto.ExameDTO;
+import com.system.angels.dto.RemoverExameDTO;
+import com.system.angels.dto.VisualizarExameDTO;
+
 public interface iExameService {
+    ExameDTO criarExame(ExameDTO exameDTO);
+    VisualizarExameDTO visualizarExame(Long id);
+
+    AtualizarExameDTO atualizarExame(AtualizarExameDTO atualizarExameDTO);
+
+    void removerExame(RemoverExameDTO removerExameDTO);
 }
--- angels/src/main/java/com/system/angels/service/impl/ExameService.java
+++ angels/src/main/java/com/system/angels/service/impl/ExameService.java
@@ -1,4 +1,29 @@
 package com.system.angels.service.impl;
 
-public class ExameService {
+import com.system.angels.dto.AtualizarExameDTO;
+import com.system.angels.dto.ExameDTO;
+import com.system.angels.dto.RemoverExameDTO;
+import com.system.angels.dto.VisualizarExameDTO;
+import com.system.angels.service.iExameService;
+
+public class ExameService implements iExameService {
+    @Override
+    public ExameDTO criarExame(ExameDTO exameDTO) {
+        return null;
+    }
+
+    @Override
+    public VisualizarExameDTO visualizarExame(Long id) {
+        return null;
+    }
+
+    @Override
+    public AtualizarExameDTO atualizarExame(AtualizarExameDTO atualizarExameDTO) {
+        return null;
+    }
+
+    @Override
+    public void removerExame(RemoverExameDTO removerExameDTO) {
+
+    }
 }
