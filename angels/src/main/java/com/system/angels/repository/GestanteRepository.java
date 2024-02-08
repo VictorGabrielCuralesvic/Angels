08b4e8d6322a76796fdc5a6d0151c19906b82611
rikcav <henriquearau000@gmail.com>
2024-02-08 09:31:34 -0300

    feat: filtro de busca por cpf

--- angels/src/main/java/com/system/angels/repository/GestanteRepository.java
+++ angels/src/main/java/com/system/angels/repository/GestanteRepository.java
@@ -3,7 +3,10 @@ package com.system.angels.repository;
 import com.system.angels.domain.Gestante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
 interface GestanteRepository extends JpaRepository<Gestante, Long> {
-
+    Optional<Gestante> findGestanteByCpf(String cpf);
 }
