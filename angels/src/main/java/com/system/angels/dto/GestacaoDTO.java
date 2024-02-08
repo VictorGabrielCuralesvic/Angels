11b3a119d7c762cad5aebb14f8dd5c22f4db6e9d
elise <Elisenovaes10@gmail.com>
2024-02-08 08:23:30 -0300

    GestacaoDTO atualizado

--- angels/src/main/java/com/system/angels/dto/GestacaoDTO.java
+++ angels/src/main/java/com/system/angels/dto/GestacaoDTO.java
@@ -1,4 +1,31 @@
 package com.system.angels.dto;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class GestacaoDTO {
+    private boolean consumoAlcool;
+    private String frequenciaAlcool;
+    private Date ultimaMenstruacao;
+    private Date gestacaoInicio;
+    private String fatorRh;
+    private boolean fuma;
+    private int cigarrosPorDia;
+    private boolean drogasUso;
+    private boolean planejada;
+    private String grupoSanguineo;
+    private int pesoPre;
+    private boolean riscoGestacional;
+    private boolean hepatiteb;
+    private String situacaoGestacional;
+
 }
