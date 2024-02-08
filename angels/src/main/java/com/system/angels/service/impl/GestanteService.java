1ddf20cee27157dc51b2e3d91a5e4a06641c0504
rikcav <henriquearau000@gmail.com>
2024-02-08 10:12:35 -0300

    feat: busca de gestante por id

--- angels/src/main/java/com/system/angels/service/impl/GestanteService.java
+++ angels/src/main/java/com/system/angels/service/impl/GestanteService.java
@@ -1,4 +1,25 @@
 package com.system.angels.service.impl;
 
-public class GestanteService {
+import com.system.angels.domain.Gestante;
+import com.system.angels.repository.GestanteRepository;
+import com.system.angels.service.iGestacaoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GestanteService implements iGestacaoService {
+    public final GestanteRepository repositorio;
+
+    public Gestante buscarGestantePorId(Long id) throws RuntimeException {
+        Optional<Gestante> gestante = repositorio.findById(id);
+
+        if (gestante.isEmpty()) {
+            throw new RuntimeException("Não existe uma gestante associada a esse id.");
+        }
+
+        return gestante.get();
+    }
 }
