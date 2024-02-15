9cb200eeb67ea91436128461dc2ad030da589b54
rikcav <henriquearau000@gmail.com>
2024-02-15 16:42:13 -0300

    feat: listarGestantes endpoint no GestanteController

--- angels/src/main/java/com/system/angels/controller/GestanteController.java
+++ angels/src/main/java/com/system/angels/controller/GestanteController.java
@@ -1,4 +1,24 @@
 package com.system.angels.controller;
 
+import com.system.angels.domain.Gestante;
+import com.system.angels.service.impl.GestanteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/gestantes")
+@RequiredArgsConstructor
 public class GestanteController {
+    private final GestanteService service;
+
+    @GetMapping
+    public ResponseEntity<List<Gestante>> listarGestantes() {
+        List<Gestante> gestantes = service.listarGestantes();
+        return ResponseEntity.ok(gestantes);
+    }
 }
