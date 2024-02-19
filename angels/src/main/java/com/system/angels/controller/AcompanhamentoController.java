e68f793e4f2e5f494f5a47a2ad3285f27dd2befb
Ronald Macario <ronaldmacario13@gmail.com>
2024-02-18 22:16:01 -0300

    feat: adicionando funcionalidade de listar acompanhamentos no controller

--- angels/src/main/java/com/system/angels/controller/AcompanhamentoController.java
+++ angels/src/main/java/com/system/angels/controller/AcompanhamentoController.java
@@ -1,4 +1,27 @@
 package com.system.angels.controller;
 
+
+import com.system.angels.domain.Acompanhamento;
+import com.system.angels.dto.CadastroAcompanhamentoDTO;
+import com.system.angels.dto.VisualizarAcompanhamentoDTO;
+import com.system.angels.service.impl.AcompanhamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/acompanhamentos")
+@RequiredArgsConstructor
 public class AcompanhamentoController {
+
+    private final AcompanhamentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Acompanhamento>> listarAcompanhamentos() {
+        List<Acompanhamento> acompanhamentos = service.listarAcompanhamentos();
+        return ResponseEntity.ok(acompanhamentos);
+    }
+
 }
