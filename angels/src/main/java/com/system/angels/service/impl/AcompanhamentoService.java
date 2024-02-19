63f64daf9d6abb3a226b7dc0246c1447a2e8b5c9
Ronald Macario <ronaldmacario13@gmail.com>
2024-02-18 22:08:48 -0300

    feat: funcionalidade de listar acompanhamentos

--- angels/src/main/java/com/system/angels/service/impl/AcompanhamentoService.java
+++ angels/src/main/java/com/system/angels/service/impl/AcompanhamentoService.java
@@ -1,4 +1,22 @@
 package com.system.angels.service.impl;
 
-public class AcompanhamentoService {
+import com.system.angels.domain.Acompanhamento;
+import com.system.angels.repository.AcompanhamentoRepository;
+import com.system.angels.service.iAcompanhamentoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class AcompanhamentoService implements iAcompanhamentoService {
+
+    private final AcompanhamentoRepository repositorio;
+
+    public List<Acompanhamento> listarAcompanhamentos() {
+        return repositorio.findAll();
+    }
+
 }
