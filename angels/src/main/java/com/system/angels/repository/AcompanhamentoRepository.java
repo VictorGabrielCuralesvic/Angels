54bc6045a3ba985f3f519f0f40357f15435fe1c4
rikcav <henriquearau000@gmail.com>
2024-02-20 19:58:11 -0300

    feat: AcompanhamentoRepository

--- angels/src/main/java/com/system/angels/repository/AcompanhamentoRepository.java
+++ angels/src/main/java/com/system/angels/repository/AcompanhamentoRepository.java
@@ -1,6 +1,5 @@
 package com.system.angels.repository;
 
-
 import com.system.angels.domain.Acompanhamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
