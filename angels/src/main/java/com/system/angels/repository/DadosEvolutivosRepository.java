85f40d84174101cad818f5e5c45cdc5e450dd60a
rikcav <henriquearau000@gmail.com>
2024-02-20 20:39:10 -0300

    feat: método para buscar dados evolutivos pela id da gestante

--- angels/src/main/java/com/system/angels/repository/DadosEvolutivosRepository.java
+++ angels/src/main/java/com/system/angels/repository/DadosEvolutivosRepository.java
@@ -4,7 +4,9 @@ import com.system.angels.domain.DadosEvolutivos;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface DadosEvolutivosRepository extends JpaRepository<DadosEvolutivos, Long> {
-    DadosEvolutivos findDadosEvolutivosByGestante_id(Long id);
+    ArrayList<DadosEvolutivos> findDadosEvolutivosByGestante_id(Long id);
 }
