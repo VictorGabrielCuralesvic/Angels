9a5f38dc518e9cea80c829ea4e787556654160b3
rikcav <henriquearau000@gmail.com>
2024-02-20 20:03:05 -0300

    feat: DadosEvolutivosService

---
+++ angels/src/main/java/com/system/angels/service/impl/DadosEvolutivosService.java
@@ -0,0 +1,10 @@
+package com.system.angels.service.impl;
+
+import com.system.angels.service.iDadosEvolutivosService;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class DadosEvolutivosService implements iDadosEvolutivosService {
+}
