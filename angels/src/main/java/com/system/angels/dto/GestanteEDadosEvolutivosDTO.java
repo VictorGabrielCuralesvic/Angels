0fdcfbbd4c43f89409b06e07f2d5f9c397738117
rikcav <henriquearau000@gmail.com>
2024-02-29 11:29:14 -0300

    fix: mudança no DTO de retorno da gestante e dados evolutivos

--- angels/src/main/java/com/system/angels/dto/GestanteEDadosEvolutivosDTO.java
+++ angels/src/main/java/com/system/angels/dto/GestanteEDadosEvolutivosDTO.java
@@ -1,11 +1,20 @@
 package com.system.angels.dto;
 
-public class GestanteEDadosEvolutivosDTO {
-    private GestanteCadastroDTO gestanteDTO;
-    private CadastrarDadosEvolutivosDTO dadosEvolutivosDTO;
+import com.system.angels.domain.DadosEvolutivos;
+import com.system.angels.domain.Gestante;
+import jakarta.persistence.Id;
+import lombok.Data;
 
-    public GestanteEDadosEvolutivosDTO(GestanteCadastroDTO gestanteDTO, CadastrarDadosEvolutivosDTO dadosEvolutivosDTO) {
-        this.gestanteDTO = gestanteDTO;
-        this.dadosEvolutivosDTO = dadosEvolutivosDTO;
+@Data
+public class GestanteEDadosEvolutivosDTO {
+    @Id
+    private Long id;
+
+    private Gestante gestante;
+    private DadosEvolutivos dadosEvolutivos;
+
+    public GestanteEDadosEvolutivosDTO(Gestante gestante, DadosEvolutivos dadosEvolutivos) {
+        this.gestante = gestante;
+        this.dadosEvolutivos = dadosEvolutivos;
     }
 }
