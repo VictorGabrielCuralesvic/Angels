4c3b23e968d233ba76a6d661bf74a940c4b4f7e4
rikcav <henriquearau000@gmail.com>
2024-02-29 11:28:22 -0300

    refactor: mudança de dados ativos para dados evolutivos

--- angels/src/main/java/com/system/angels/repository/DadosAtivosRepository.java
+++
@@ -1,7 +0,0 @@
-package com.system.angels.repository;
-
-import com.system.angels.domain.DadosAtivos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DadosAtivosRepository extends JpaRepository<DadosAtivos, Long> {
-}
--- angels/src/main/java/com/system/angels/service/iDadosAtivosService.java
+++
@@ -1,9 +0,0 @@
-package com.system.angels.service;
-
-import com.system.angels.dto.CadastrarDadosAtivosDTO;
-
-public interface iDadosAtivosService {
-
-    CadastrarDadosAtivosDTO criarDadosAtivos(CadastrarDadosAtivosDTO cadastrarDadosAtivosDTO);
-
-}
