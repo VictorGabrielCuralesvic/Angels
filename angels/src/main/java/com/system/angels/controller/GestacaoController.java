1a14d3a89db11592422aa852ffd75b0f39c5b446
rikcav <henriquearau000@gmail.com>
2024-03-10 15:26:08 -0300

    fix: import de PutMapping

--- angels/src/main/java/com/system/angels/controller/GestacaoController.java
+++ angels/src/main/java/com/system/angels/controller/GestacaoController.java
@@ -9,13 +9,7 @@ import com.system.angels.service.impl.GestanteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.system.angels.domain.Gestacao;
 import com.system.angels.dto.update.AtualizarGestacaoDTO;
